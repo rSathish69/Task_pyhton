741d5c46ef229d91814ba08b04b881a8becc6bd9
rSathish69 <111122888+rSathish69@users.noreply.github.com>
2022-08-12 18:16:18 +0530

    Add files via upload

---
+++ firstprogram.py
@@ -0,0 +1,97 @@
+# Marks List
+
+marks = [16,25,31,35,45,"sathish[40]",59,65,71,82,95,99,100]
+
+# taking input
+# if a pass marks is above 50
+
+User = input("Did you want to know how many student wrote the exam?")
+
+total_student = len(marks)
+
+print(total_student)
+
+print("There are " + str(total_student) +" student wrote the exam")
+
+# find a specify student mark
+
+user = input("mention the name if you want to know his mark : ")
+
+print( "he got only " + str(marks[6]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
